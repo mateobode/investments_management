cbced0fc0da74f90e45da50327653145a064e479
mateobode <mateo_bode@yahoo.com>
2023-02-15 16:16:37 +0100

    updated model fields and properties

--- backend/models/cashflow.py
+++ backend/models/cashflow.py
@@ -5,7 +5,7 @@ from backend.utils import TYPES
 
 
 class Cashflow(models.Model):
-    loan_identifier = models.ForeignKey(Loan, related_name="loans", on_delete=models.CASCADE)
+    loan_identifier = models.ForeignKey(Loan, related_name="cash_flows", on_delete=models.CASCADE)
     reference_date = models.DateField()
     type = models.CharField(choices=TYPES, max_length=10)
     amount = models.FloatField()
--- backend/models/loan.py
+++ backend/models/loan.py
@@ -1,5 +1,8 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import Sum
+from django.db.models.functions import Cast
+from xirr.math import xirr
 
 
 class Loan(models.Model):
@@ -15,25 +18,45 @@ class Loan(models.Model):
 
     @property
     def invested_amount(self):
-        return [loan.loans.values('amount') for loan in self.objects.filter(loans__type__exact="funding")]
+        return abs(self.cash_flows.filter(type="funding").aggregate(Sum('amount')).get("amount__sum"))
 
     @property
     def investment_date(self):
-        pass
-        #return [loan.loans.values[] for loan in self.objects.filter(loans__reference_date=)]
+        return self.cash_flows.filter(type="funding").annotate(
+            investment_date=Cast(
+                "reference_date",
+                output_field=models.CharField()
+            )
+        ).values_list('investment_date', flat=True).get()
 
     @property
     def expected_interest_amount(self):
-        pass
+        return self.total_expected_interest_amount * (self.invested_amount / self.total_amount)
 
     @property
     def is_closed(self):
-        pass
+        if self.cash_flows.filter(type="repayment").aggregate(
+                Sum("amount")
+        ).get('amount__sum') >= (self.invested_amount + self.expected_interest_amount):
+            return True
+        else:
+            return False
 
     @property
     def expected_irr(self):
-        pass
+        reference_date = self.cash_flows.filter(type="funding").values_list("reference_date").get()[0]
+        funding_amount = self.cash_flows.filter(type="funding").values_list("amount").get()[0]
+        maturity_date = self.maturity_date
+        expected_repayment_amount = self.invested_amount + self.expected_interest_amount
+        expected_irr = {reference_date: funding_amount, maturity_date: expected_repayment_amount}
+        return xirr(expected_irr)
 
     @property
     def realized_irr(self):
-        pass
+        if self.is_closed:
+            funding_reference_date = self.cash_flows.filter(type="funding").values_list("reference_date").get()[0]
+            funding_amount = self.cash_flows.filter(type="funding").values_list("amount").get()[0]
+            repayment_reference_date = self.cash_flows.filter(type="repayment").values_list("reference_date").get()[0]
+            repayment_amount = self.cash_flows.filter(type="repayment").values_list("amount").get()[0]
+            realized_irr = {funding_reference_date: funding_amount, repayment_reference_date: repayment_amount}
+            return xirr(realized_irr)
