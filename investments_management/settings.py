ad8df470b157abebbeaf7ffb1de5d4814a0c2d3e
mateobode <mateo_bode@yahoo.com>
2023-02-24 15:56:56 +0100

    updated database and settings file

--- investments_management/settings.py
+++ investments_management/settings.py
@@ -131,5 +131,5 @@ REST_FRAMEWORK = {
 }
 
 # Celery settings
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
